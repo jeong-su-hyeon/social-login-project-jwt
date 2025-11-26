ec43925ccf42653b2a35f136725a685ae850329c
suhyeon <102345466+jeong-su-hyeon@users.noreply.github.com>
2025-11-26 17:25:36 +0900

    chore: gradle 프로젝트 초기

---
+++ src/main/java/com/example/social_login_project_gradle/SocialLoginProjectGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.social_login_project_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginProjectGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialLoginProjectGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/social_login_project_gradle/SocialLoginProjectGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.social_login_project_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginProjectGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
